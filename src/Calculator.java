c7967b11c316b274887739d53ecaa5ea7a3557b6
Roman <rgrabelnikov@yandex.ru>
2020-03-23 15:28:44 +0700

    Made calculator with spaces around operations symbols

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,49 +5,32 @@ public class Calculator {
   private String regExp1 = "[/\\*]";
   private String regExp2 = "[-\\+]";
 
-  public int[] getNumbersBounds(String[] strArr, int index) {
-    int startNum1 = 0;
-    int endNum1 = index - 1;
-    for (int j = index - 1; j > 0; j--) {
-      if (strArr[j].matches(this.regExp2) || strArr[j].matches(this.regExp1)) {       // remove second check??
-        startNum1 = j + 1;
-        break;
-      }
-    }
-
-    int startNum2 = index + 1;
-    int endNum2 = strArr.length - 1;
-    for (int j = index + 1; j < strArr.length; j++) {
-      if (strArr[j].matches(this.regExp2) || strArr[j].matches(this.regExp1)) {       // remove second check??
-        endNum2 = j - 1;
-        break;
-      }
-    }
-
-    return new int[]{ startNum1, endNum1, startNum2, endNum2 };
-  }
-
   public double calculate(String str) throws Exception {
-    Pattern pattern = Pattern.compile("[-/\\*\\+]");
-    Matcher matcher = pattern.matcher(str);
-    if (!matcher.find()) {
-      System.out.println("answer");
-      return Double.parseDouble(str);
+    Pattern pattern1 = Pattern.compile("\\d+?");
+    Matcher matcher1 = pattern1.matcher(str);
+    if (!matcher1.find())
+      throw new Exception("Выражение введено неверно");
+    Pattern patternComma = Pattern.compile("\\d+?,\\d+?");
+    Matcher matcherComma = patternComma.matcher(str);
+    if (matcherComma.find())
+      throw new Exception("Неверный ввод нецелочисленного числа (десятичные числа должны вводиться через \".\")");
+    Pattern patternTogether = Pattern.compile("(\\d+?\\+\\d+?)|(\\d+?-\\d+?)|(\\d+?\\*\\d+?)|(\\d+?/\\d+?)");
+    Matcher matcherTogether = patternTogether.matcher(str);
+    if (matcherTogether.find())
+      throw new Exception("Выражение введено неверно (символы операций должны обосабливаться пробелами)");
+
+    Pattern patternOperation = Pattern.compile("(\\s-\\s)|(\\s/\\s)|(\\s\\*\\s)|(\\s\\+\\s)");
+    Matcher matcherOperation = patternOperation.matcher(str);
+    if (!matcherOperation.find()) {
+      double answer = Math.round(Double.parseDouble(str) * 1000000);
+      return answer / 1000000;
     }
 
-    String[] strArr = str.split("");
+    String[] strArr = str.split("\\s");
     for (int i = 0; i < strArr.length; i++) {
       if (strArr[i].matches(this.regExp1)) {
-        int[] numbersBounds = this.getNumbersBounds(strArr, i);
-        String numStr1 = "";
-        for (int j = numbersBounds[0]; j <= numbersBounds[1]; j++)
-          numStr1 += strArr[j];
-        double num1 = Double.parseDouble(numStr1);
-        String numStr2 = "";
-        for (int j = numbersBounds[2]; j <= numbersBounds[3]; j++)
-          numStr2 += strArr[j];
-        double num2 = Double.parseDouble(numStr2);
-
+        double num1 = Double.parseDouble(strArr[i - 1]);
+        double num2 = Double.parseDouble(strArr[i + 1]);
         double result;
         if (strArr[i].matches("\\*"))
           result = num1 * num2;
@@ -57,24 +40,21 @@ public class Calculator {
           result = num1 / num2;
 
         String newStr = "";
-        for (int j = 0; j < numbersBounds[0]; j++)
-          newStr += strArr[j];
-        newStr += Double.toString(result);
-        for (int j = numbersBounds[3] + 1; j < strArr.length; j++)
-          newStr += strArr[j];
-//        System.out.println("newstr1: " + newStr);
+        for (int j = 0; j < i - 1; j++)
+          newStr += strArr[j] + " ";
+        newStr += Double.toString(result) + " ";
+        for (int j = i + 2; j < strArr.length; j++)
+          newStr += strArr[j] + " ";
+        newStr = newStr.substring(0, newStr.length() - 1);
+        System.out.println(newStr);
         return this.calculate(newStr);
-      } else if (strArr[i].matches(this.regExp2)) {
-        int[] numbersBounds = this.getNumbersBounds(strArr, i);
-        String numStr1 = "";
-        for (int j = numbersBounds[0]; j <= numbersBounds[1]; j++)
-          numStr1 += strArr[j];
-        double num1 = Double.parseDouble(numStr1);
-        String numStr2 = "";
-        for (int j = numbersBounds[2]; j <= numbersBounds[3]; j++)
-          numStr2 += strArr[j];
-        double num2 = Double.parseDouble(numStr2);
+      }
+    }
 
+    for (int i = 0; i < strArr.length; i++) {
+      if (strArr[i].matches(this.regExp2)) {
+        double num1 = Double.parseDouble(strArr[i - 1]);
+        double num2 = Double.parseDouble(strArr[i + 1]);
         double result;
         if (strArr[i].matches("\\+"))
           result = num1 + num2;
@@ -82,18 +62,16 @@ public class Calculator {
           result = num1 - num2;
 
         String newStr = "";
-        for (int j = 0; j < numbersBounds[0]; j++)
-          newStr += strArr[j];
-        newStr += Double.toString(result);
-        for (int j = numbersBounds[3] + 1; j < strArr.length; j++)
-          newStr += strArr[j];
-//        System.out.println("newstr2: " + newStr);
+        for (int j = 0; j < i - 2; j++)
+          newStr += strArr[j] + " ";
+        newStr += Double.toString(result) + " ";
+        for (int j = i + 2; j < strArr.length; j++)
+          newStr += strArr[j] + " ";
+        newStr = newStr.substring(0, newStr.length() - 1);
+        System.out.println(newStr);
         return this.calculate(newStr);
       }
     }
-
-//    System.out.println(str);
-//    return Double.parseDouble(str);
     return -1;
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,16 @@ public class Main {
     Calculator calc = new Calculator();
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Введите выражение (без пробелов):");
+    System.out.println("Введите выражение (символы операций должны обосабливаться пробелами):");
     String expression = sc.nextLine();
 
-//    try {
+    try {
       double answer = calc.calculate(expression);
-
       System.out.println("Ответ: " + answer);
-
       sc.close();
-//    } catch (Exception e) {
-//      System.out.println("Ошибка: " + e.getMessage());
-//    }
+    } catch (Exception e) {
+      System.out.println("Ошибка: " + e.getMessage());
+      e.printStackTrace();
+    }
   }
 }
